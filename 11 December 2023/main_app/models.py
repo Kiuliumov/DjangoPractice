9d87836460bffaa684a7edaf567b34930177c1d0
Kiuliumov <miti.bania@gmail.com>
2026-02-09 09:34:21 +0200

    SU

--- 11 December 2023/main_app/models.py
+++ 11 December 2023/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class TennisPlayer(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(5)])
+    birth_date = models.DateField()
+    country = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+
