759772234b9175ecfef7eb2fdc58beb2bd031248
Kiuliumov <miti.bania@gmail.com>
2026-02-11 23:47:54 +0200

    SU

--- 26 November 2023/main_app/models.py
+++ 26 November 2023/main_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 
-
 # Create your models here.
 
 class Author(models.Model):
