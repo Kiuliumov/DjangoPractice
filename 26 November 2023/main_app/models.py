01f53fc7ec914cb92fdddbb35ae476eb5193c269
Kiuliumov <miti.bania@gmail.com>
2026-02-11 16:48:21 +0200

    SU

--- 26 November 2023/main_app/models.py
+++ 26 November 2023/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    email = models.EmailField(unique=True)
+    is_banned = models.BooleanField(default=False)
+    birth_year = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2005)])
+    website = models.URLField(blank=True, null=True)
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    category = models.CharField(max_length=10, choices=(('Technology', 'Technology'), ('Science', 'Science'), ('Education', 'Education')), default='Technology')
+    authors = models.ManyToManyField(Author, related_name="articles")
+    published_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Review(models.Model):
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    rating = models.FloatField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="reviews")
+    published_on = models.DateTimeField(auto_now_add=True, editable=False)
