048025aa8c922eb48a66cb0acb38103bfda7abed
Kiuliumov <miti.bania@gmail.com>
2026-02-12 09:28:18 +0200

    SU

--- 26 November 2023/caller.py
+++ 26 November 2023/caller.py
@@ -1,5 +1,7 @@
 import os
 import django
+from main_app.models import *
+from django.db.models import Count, Avg, Q
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -8,3 +10,33 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
+
+def get_authors(search_name=None, search_email=None):
+    if not search_name and not search_email:
+        return ""
+    query = Q()
+    if search_name:
+        query &= Q(full_name__icontains=search_name)
+
+    if search_email:
+        query &= Q(email__icontains=search_email)
+    authors = Author.objects.filter(query).order_by('-full_name')
+
+    if not authors:
+        return ''
+    lines = [f'Author: {author.full_name}, email: {author.email}, status: {"Banned" if author.is_banned else "Not Banned"}' for author in authors]
+    return '\n'.join(lines)
+
+def get_top_publisher():
+    top_publisher = Author.objects.annotate(article_count=Count('articles')).order_by('-article_count', 'email').first()
+
+    if not top_publisher or top_publisher.article_count == 0:
+        return ''
+
+    return f'Top Author: {top_publisher.full_name} with {top_publisher.article_count} published articles.'
+
+def get_top_reviewer():
+    top_reviewer = Author.objects.annotate(review_count=Count('reviews')).order_by('-review_count', 'email').first()
+    if not top_reviewer or top_reviewer.review_count == 0:
+        return ''
+    return f'Top Reviewer: {top_reviewer.full_name} with {top_reviewer.review_count} reviews.'
