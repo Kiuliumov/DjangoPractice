603e840fc7aca2b4d5eaa025b8097205e7ca969c
Kiuliumov <miti.bania@gmail.com>
2026-02-12 09:55:04 +0200

    SU

--- 26 November 2023/caller.py
+++ 26 November 2023/caller.py
@@ -40,3 +40,54 @@ def get_top_reviewer():
     if not top_reviewer or top_reviewer.review_count == 0:
         return ''
     return f'Top Reviewer: {top_reviewer.full_name} with {top_reviewer.review_count} published reviews.'
+
+def get_latest_article():
+    latest_article = (
+        Article.objects
+        .order_by('-date_published')
+        .annotate(
+            review_count=Count('reviews'),
+            avg_rating=Avg('reviews__rating')
+        )
+        .first()
+    )
+
+    if not latest_article:
+        return ''
+
+    return (
+        f'The latest article is: {latest_article.title}. '
+        f'Authors: {", ".join(a.full_name for a in latest_article.authors.all())}. '
+        f'Reviewed: {latest_article.review_count} times. '
+        f'Average Rating: {latest_article.avg_rating or 0:.2f}.'
+    )
+
+def get_top_rated_article():
+
+    top_rated_article = (Article.objects
+                        .annotate(review_count=Count('reviews'), avg_rating=Avg('reviews__rating'))
+                        .order_by('-avg_rating', 'title')
+                        .first())
+
+    if not top_rated_article:
+        return ''
+
+    if not top_rated_article.reviews.exists():
+        return ''
+
+    return f'The top-rated article is: {top_rated_article.title}, with an average rating of {top_rated_article.avg_rating}, reviewed {top_rated_article.review_count} times.'
+
+def ban_author(email=None):
+    if not email:
+        return 'No authors banned.'
+    author = Author.objects.filter(email__iexact=email).annotate(reviews_count=Count('reviews')).first()
+
+    if not author:
+        return 'No authors banned.'
+
+    reviews_count = author.reviews_count
+    author.is_banned = True
+    author.reviews.all().delete()
+
+    author.save()
+    return f'Author: {author.full_name} is banned! {reviews_count} reviews were deleted. '
