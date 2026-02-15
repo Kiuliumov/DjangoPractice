25a90820f0af54d167a87302b4173ae4923a2fb4
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-15 21:54:04 +0200

    asdf

--- Exam Prep II/main_app/admin.py
+++ Exam Prep II/main_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number', 'is_active')
+    search_fields = ('full_name', 'email')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'in_stock', 'is_available')
+    filters = ('is_available',)
+    search_fields = ('name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'total_price', 'creation_date', 'is_completed')
+    filters = ('is_completed',)
+    search_fields = ('profile.full_name', )
