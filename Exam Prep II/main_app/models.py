658573f0b34e99db07237a1e46d49f91d64a8423
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-16 08:17:23 +0200

    asdf

--- Exam Prep II/main_app/models.py
+++ Exam Prep II/main_app/models.py
@@ -9,7 +9,7 @@ class ProfileManager(models.Manager):
     def get_regular_customers(self):
         return (self.get_queryset()
                 .annotate(orders_count=Count("orders"))
-                .filter(orders_count__gte=2)
+                .filter(orders_count__gt=2)
                 .order_by('-orders_count'))
 
 class Profile(models.Model):
